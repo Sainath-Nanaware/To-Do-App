36db64cfb684bbe77e3043b880b9f7caddffeb1c
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-01-12 23:52:01 +0530

    resolve image bug

--- reactApp/src/Components/UserInfo/UserInfo.jsx
+++ reactApp/src/Components/UserInfo/UserInfo.jsx
@@ -3,7 +3,7 @@ import "./userInfo.css"
 function UserInfo() {
   return (
     <div className='userInfo'>
-            <img src="https://i1.sndcdn.com/avatars-000814566505-400mc7-t500x500.jpg" alt="" />
+            <img src="./s_email.logo.jpg" alt="" />
             <p>Sainath  Nanaware</p>
     </div>
   )
